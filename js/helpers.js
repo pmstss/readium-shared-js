a928ea63b57c74202383dc8b45be7dc2cf62e688
Juan Corona <juanc@evidentpoint.com>
2014-03-18 15:52:27 -0700

    Add helper functions for cross browser CSS Transformation Matrix parsing.

--- js/helpers.js
+++ js/helpers.js
@@ -263,3 +263,20 @@ ReadiumSDK.Helpers.escapeJQuerySelector = function(sel) {
     // escapeSelector('|');
     // escapeSelector('}');
     // escapeSelector('~');
+
+
+ReadiumSDK.Helpers.CSSTransformMatrix = {
+    getMatrix: function ($obj) {
+        var matrix = $obj.css("-webkit-transform") ||
+            $obj.css("-moz-transform") ||
+            $obj.css("-ms-transform") ||
+            $obj.css("-o-transform") ||
+            $obj.css("transform");
+        return matrix;
+    },
+    getScaleFromMatrix: function (matrix) {
+        var matrixRegex = /matrix\((-?\d*\.?\d+),\s*0,\s*0,\s*(-?\d*\.?\d+),\s*0,\s*0\)/,
+            matches = matrix.match(matrixRegex);
+        return matches;
+    }
+};
