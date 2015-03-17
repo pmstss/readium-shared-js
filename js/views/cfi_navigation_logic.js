bdcc2204086c51723633309695e0324aa64f2a76
Juan Corona <juanc@evidentpoint.com>
2015-03-17 12:32:57 -0700

    The cfi must be wrapped to obtain the target element

--- js/views/cfi_navigation_logic.js
+++ js/views/cfi_navigation_logic.js
@@ -929,7 +929,7 @@ ReadiumSDK.Views.CfiNavigationLogic = function ($viewport, $iframe, options) {
     }
 
     function getTargetElementFromCfi(cfi) {
-        return EPUBcfi.getTargetElementWithPartialCFI(cfi);
+        return EPUBcfi.getTargetElementWithPartialCFI(getWrappedCfi(cfi));
     }
 
     this.getDomRangeFromRangeCfi = function(rangeCfi, rangeCfi2, inclusive) {
