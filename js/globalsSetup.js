c31a0e045475977ca8c835a6aec4603ab16a3e8f
Juan Corona <juanc@evidentpoint.com>
2015-09-02 18:03:54 -0700

    Clean up included libraries. Move es6-collections lib over to requirejs config as an NPM module.

--- js/globalsSetup.js
+++ js/globalsSetup.js
@@ -12,7 +12,7 @@
 //  prior written permission.
 
 //'text!empty:'
-define(['console_shim', 'eventEmitter', 'URIjs', 'readium_cfi_js', 'readium_js_plugins', './globals'], function (console_shim, EventEmitter, URI, epubCfi, PluginsController, Globals) {
+define(['console_shim', 'es6-collections', 'eventEmitter', 'URIjs', 'readium_cfi_js', 'readium_js_plugins', './globals'], function (console_shim, es6collections, EventEmitter, URI, epubCfi, PluginsController, Globals) {
 
     console.log("Globals...");
 
